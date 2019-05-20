306096b09f0bac9b70c714de6476d74019aeabaf
Seweryn Karecki <seweryn@authenticstyle.co.uk>
2019-05-20 15:19:00 +0100

    homepage done

---
+++ precache-manifest.cf1085458eae26d072a12d341d354fae.js
@@ -0,0 +1,75 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f6d15ebb01fd8a4d5c1e8ce4bbf8cb",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.cf42dd4f2d16f66a9837.bundle.js"
+  },
+  {
+    "revision": "2b00468d0f75bde9ce78",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.cf42dd4f2d16f66a9837.bundle.js"
+  },
+  {
+    "revision": "6cae5804d22a96aa5a0233cb7abe72b3",
+    "url": "static/media/as-logo.6cae5804.png"
+  },
+  {
+    "revision": "e44504d9f9da9ad945b9be2752671ac3",
+    "url": "static/media/charterBG.e44504d9.png"
+  },
+  {
+    "revision": "b065346dcadd73d1666cbdbdd3bc7eb7",
+    "url": "static/media/charterImg1.b065346d.png"
+  },
+  {
+    "revision": "5318c0978be60b761279da10db232023",
+    "url": "static/media/charterImg2.5318c097.png"
+  },
+  {
+    "revision": "36c30b60f8959166a9964401a2f6a4e6",
+    "url": "static/media/charterImg3.36c30b60.png"
+  },
+  {
+    "revision": "28bb5d1846a5acfc9a345570d83d207c",
+    "url": "static/media/expImg1.28bb5d18.png"
+  },
+  {
+    "revision": "91c787a70ab61d0896474e98076bf4f6",
+    "url": "static/media/expImg2.91c787a7.png"
+  },
+  {
+    "revision": "84e3bc34c34d8157f53ff0c4f75fe7d0",
+    "url": "static/media/featBoat1.84e3bc34.png"
+  },
+  {
+    "revision": "eb69959849090d45bace07d8b38b625f",
+    "url": "static/media/featBoat2.eb699598.png"
+  },
+  {
+    "revision": "72084f330c946c283b4a6b332961823e",
+    "url": "static/media/featBoat3.72084f33.png"
+  },
+  {
+    "revision": "e7d93f8b035b1efa328bea8391c52a1b",
+    "url": "static/media/hero.e7d93f8b.png"
+  },
+  {
+    "revision": "3cb08333e4da5d87db5d1c9321e0a401",
+    "url": "static/media/logo.3cb08333.png"
+  },
+  {
+    "revision": "e524b15d9c1750893f3c0636563afb56",
+    "url": "static/media/slider.e524b15d.png"
+  },
+  {
+    "url": "vendors~main.cf42dd4f2d16f66a9837.bundle.js"
+  },
+  {
+    "revision": "e934d882df94c1120bff",
+    "url": "vendors~main.css"
+  }
+]);
